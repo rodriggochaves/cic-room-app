d4126d89d677000d9633a108c81f8035a078d947
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2017-11-23 09:07:15 -0200

    promt para inserir o nome antes de entrar na sala

--- src/pages/list-room/list-room.ts
+++ src/pages/list-room/list-room.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 // other pages
 import { WaitingRoomPage } from '../../pages/waiting-room/waiting-room';
@@ -14,10 +15,35 @@ import { NewRoomPage } from '../../pages/new-room/new-room';
 export class ListRoomPage {
 
   constructor(public navCtrl: NavController, 
-              public navParams: NavParams) {}
+              public navParams: NavParams,
+              public alertCtrl: AlertController) {}
 
   enterRoom() {
-    this.navCtrl.push(WaitingRoomPage);
+    let alert = this.alertCtrl.create({
+      title: 'Confirme sua entrada!',
+      inputs: [ { name: 'name', placeholder: 'Nome' } ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Entrar!',
+          handler: data => {
+            // enviar requisicao para o server com a pessoa entrando na sala
+            let navTransition = alert.dismiss().then(() => {
+              this.navCtrl.setRoot(WaitingRoomPage);
+            });
+            return false;
+          }
+        }
+      ]
+    });
+
+    alert.present();
   }
 
   newRoomPage() {
